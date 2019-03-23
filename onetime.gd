9b39aa9bd2a4172a30b7341eae0c31ddbe7c87de
Adrian Watson Gordillo <adrian.watsongordillo@student.manchester.ac.uk>
2019-03-23 18:52:04 +0000

    enlarged level

--- onetime.gd
+++ onetime.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 # Declare member variables here. Examples
 var collider
-var normal_platform = load("res://long_obstacle.png")
+# var normal_platform = load("res://img/safetynet.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_exited", self, "body_exited")
+	get_node("Sprite").visible = false;
 	collider = get_node("Collider")
 	collider.collision_layer = 0
 	collider.collision_mask = 0
@@ -16,6 +17,7 @@ func _ready():
 func body_exited(body):
 	print("hi")
 	if body.has_method("out"): # Only players can be out
-		get_node("Sprite").texture = normal_platform
+		#get_node("Sprite").texture = normal_platform
+		get_node("Sprite").visible = true;
 		collider.collision_layer = 1
 		collider.collision_mask = 1
