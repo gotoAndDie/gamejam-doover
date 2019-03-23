1cf92127905da1b384db1d7da212b2ceecc2ddac
Leo <chun.huang@student.manchester.ac.uk>
2019-03-23 17:01:08 +0000

    Freeze upon time over

--- colworld.gd
+++ colworld.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var initPos = Vector2(233, 223)
+var theGui
 
 func _on_princess_body_enter(body):
 	# The name of this editor-generated callback is unfortunate
@@ -8,13 +9,21 @@ func _on_princess_body_enter(body):
 		$youwin.show()
 		
 func _ready():
+	var gui = load("res://GUI.tscn") 
+	theGui = gui.instance();
+	
+	add_child(theGui)
 	for N in get_children():
 		if "Coin" in N.get_name():
 			N.connect("collect", self, "getCoin")
 	get_node("player").connect("death", self, "die")
+	theGui.get_node("MarginContainer/HBoxContainer/VBoxContainer/CashText").connect("freeze", self, "freezePlayer")
 
 func die():
-	get_node("GUI/MarginContainer/HBoxContainer2/VBoxContainer2/DeathText").die()
+	theGui.get_node("MarginContainer/HBoxContainer2/VBoxContainer2/DeathText").die()
 	
 func getCoin():
-	get_node("GUI/MarginContainer/HBoxContainer/VBoxContainer/CashText").getCoin()
+	theGui.get_node("MarginContainer/HBoxContainer/VBoxContainer/CashText").getCoin()
+	
+func freezePlayer():
+	get_node("player").freeze()
